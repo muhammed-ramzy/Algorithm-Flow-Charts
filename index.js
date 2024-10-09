89112b48fcbb7398fd1c96789c9ae3b932d8f372
muhammed-ramzy <Muhammedabdelfattah89@gmail.com>
2024-10-09 10:21:09 +0300

    initial commit

---
+++ index.js
@@ -0,0 +1,21 @@
+//Calculating the area of Parallelogram
+/*
+Inputs:
+    base , height
+
+Processes:
+    Area = base * height
+
+Output:
+        Area
+*/
+
+
+
+
+
+
+
+
+
+//Calculating the area of Trapezoid
